9cf0be4892d637273826d046aa1fd562aa5426fb
孤雁幽幽 <guyanyouyou@163.com>
2019-08-01 17:36:21 +0800

    'laravel前后端分离项目基础代码'

---
+++ app/Http/Controllers/API/CafesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Cafe;
+
+class CafesController extends Controller
+{
+    public function getCafes(){
+        $cafes = Cafe::all();
+        return response()->json($cafes);
+    }
+
+    public function postNewCafe(){
+        $cafe = new Cafe();
+        $cafe->name = Request::get('name');
+        $cafe->address = Request::get('address');
+        $cafe->city = Request::get('city');
+        $cafe->state = Request::get('state');
+        $cafe->zip = Request::get('zip');
+        $cafe->save();
+        return response()->json($cafe,201);
+    }
+
+    public function getCafe($id){
+        $cafe = Cafe::where('id','=',$id)->first();
+        return response()->json($cafe);
+    }
+}
---
+++ resources/assets/js/pages/Home.vue
@@ -0,0 +1,29 @@
+<style>
+
+</style>
+
+<template>
+    <div>
+        <span v-show="cafesLoadStatus == 1">加载中...</span>
+        <span v-show="cafesLoadStatus == 2">数据加载成功！</span>
+        <span v-show="cafesLoadStatus == 3">数据加载失败！</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        created() {
+            this.$store.dispatch('loadCafes');
+        },
+        computed:{
+            // 获取cafes加载状态
+            cafesLoadStatus(){
+                return this.$store.getters.getCafesLoadStatus;
+            },
+            //获取cafes
+            cafes(){
+                return this.$store.getters.getCafes;
+            }
+        }
+    }
+</script>
