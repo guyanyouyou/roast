d6f949f6c67f16e26c6661e846ea3490a59dae86
孤雁幽幽 <guyanyouyou@163.com>
2019-08-03 17:58:44 +0800

    通过 Vue.js 实现动态表单一次提交多个咖啡店位置信息

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -16,15 +16,81 @@ class CafesController extends Controller
     }
 
     public function postNewCafe(StoreCafeRequest $request){
+        //已添加的咖啡店
+        $addedCafes = [];
+        //所有位置信息
+        $locations = $request->input('locations');
+        //父节点（可理解为总店）
+        $parentCafe = new Cafe();
+
+        //咖啡店名称
+        $parentCafe->name = $request->input('name');
+        //分店位置名称
+        $parentCafe->location_name = $locations[0]['name']?:'';
+        //分店地址
+        $parentCafe->address = $locations[0]['address'];
+        //所在城市
+        $parentCafe->city = $locations[0]['city'];
+        //所在省份
+        $parentCafe->state = $locations[0]['state'];
+        //邮政编码
+        $parentCafe->zip = $locations[0]['zip'];
+        $coordinates = GaodeMaps::geocodeAddress($parentCafe->address,$parentCafe->city,$parentCafe->state);
+        //纬度
+        $parentCafe->latitude = $coordinates['lat'];
+        //经度
+        $parentCafe->longitude = $coordinates['lng'];
+        //咖啡烘培师
+        $parentCafe->roaster = $request->input('roaster')?1:0;
+        //咖啡店网址
+        $parentCafe->website = $request->input('website');
+        //描述信息
+        $parentCafe->description = $request->input('description')?:'';
+        //添加者
+        $parentCafe->added_by = $request->user()->id;
+        $parentCafe->save();
+
+        //冲泡方法
+        $brewMethods = $locations[0]['methodsAvailable'];
+        //保存与此咖啡店关联的所有冲泡方法(保存关联关系)
+        $parentCafe->brewMethods()->sync($brewMethods);
+
+        //将当前咖啡店数据推送到已添加咖啡店数组
+        array_push($addedCafes,$parentCafe->toArray());
+
+        //第一个索引的位置信息已经使用，从第二个位置开始
+        if (count($locations) > 1){
+            //从索引1开始
+            for ($i=1;$i<count($locations);$i++){
+                //其它分店信息的获取和保存，与总店公用名称、网址、描述、烘培师等信息，其他逻辑与总店一致
+                $cafe = new Cafe();
+                $cafe->parent = $parentCafe->id;
+                $cafe->name = $request->input('name');
+                $cafe->location_name = $locations[$i]['name']?:'';
+                $cafe->address = $locations[$i]['address'];
+                $cafe->city = $locations[$i]['city'];
+                $cafe->state = $locations[$i]['state'];
+                $cafe->zip = $locations[$i]['zip'];
+                $coordinates = GaodeMaps::geocodeAddress($cafe->address,$cafe->city,$cafe->state);
+                $cafe->latitude = $coordinates['lat'];
+                $cafe->longitude  = $coordinates['lng'];
+                $cafe->roaster = $request->input('roaster')!=''?1:0;
+                $cafe->website = $request->input('website');
+                $cafe->description = $request->input('description')?:'';
+                $cafe->added_by = $request->user()->id;
+
+
+                $cafe->save();
+
+                $cafe->brewMethods()->sync($locations[$i]['methodsAvailable']);
+                array_push($addedCafes,$cafe->toArray());
+            }
+        }
         $cafe = new Cafe();
-        $cafe->name = $request->input('name');
         $cafe->address = $request->input('address');
         $cafe->city = $request->input('city');
         $cafe->state = $request->input('state');
         $cafe->zip = $request->input('zip');
-        $coordinates = GaodeMaps::geocodeAddress($cafe->address,$cafe->city,$cafe->state);
-        $cafe->latitude = $coordinates['lat'];
-        $cafe->longitude = $coordinates['lng'];
         $cafe->save();
         return response()->json($cafe,201);
     }
--- resources/assets/js/api/cafe.js
+++ resources/assets/js/api/cafe.js
@@ -7,13 +7,13 @@ export default {
     getCafe:function(cafeID){
         return axios.get(ROAST_CONFIG.API_URL+'/cafes/'+cafeID)
     },
-    postAddNewCafe:function(name,address,city,state,zip){
+    postAddNewCafe:function(name,locations,website,description,roaster){
         return axios.post(ROAST_CONFIG.API_URL + '/cafes/',{
             name:name,
-            address:address,
-            city:city,
-            state:state,
-            zip:zip,
+            locations:locations,
+            website:website,
+            description:description,
+            roaster:roaster,
         })
     }
 }
