4c89e625e72575b3dfae34a0d44b49693b158ac4
孤雁幽幽 <guyanyouyou@163.com>
2019-08-05 18:39:55 +0800

    通过 Laravel + Vue 实现喜欢/取消喜欢咖啡店功能

---
+++ resources/assets/js/components/cafes/IndividualCafeMap.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "IndividualCafeMap"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/cafes/ToggleLike.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ToggleLike"
+    }
+</script>
+
+<style scoped>
+
+</style>
